8f892fe987304837169a6ff702d7d7410821e541
weedomeker <weedomeker@gmail.com>
2023-12-18 22:20:46 +0100

    change port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
